2a49527c9fada7dd7f99da45a27cf25371de3faf
Dssaenz <darwinsaenz105@gmail.com>
2020-05-19 23:49:47 -0500

    finish game

--- index.js
+++ index.js
@@ -1,35 +1,47 @@
-const celeste = document.getElementById('celeste');
-const violeta = document.getElementById('violeta');
-const naranja = document.getElementById('naranja');
-const verde = document.getElementById('verde');
-const btnEmpezar = document.getElementById("btnEmpezar");
-
+const celeste = document.getElementById('celeste')
+const violeta = document.getElementById('violeta')
+const naranja = document.getElementById('naranja')
+const verde = document.getElementById('verde')
+const btnEmpezar = document.getElementById("btnEmpezar")
+const ultimoNivel = 10;
 class Juego {
   constructor() {
-      this.inicializar();
-      this.generarSecuencia();
-      this.siguienteNivel();
+      this.inicializar = this.inicializar.bind(this)
+      this.inicializar()
+      this.generarSecuencia()
+      setTimeout(this.siguienteNivel, 500)
   }
 
   inicializar() {
+    this.siguienteNivel = this.siguienteNivel.bind(this)
     this.elegirColor = this.elegirColor.bind(this)
+    this.toggleBtnEmpezar()
       btnEmpezar.classList.add('hide');
-      this.nivel = 1;
+      this.nivel = 1
       this.colores = {
           celeste,
           violeta,
           naranja,
-          verde
+          verde,
       }
   }
   
+  toggleBtnEmpezar() {
+    if(btnEmpezar.classList.contains('hide')){
+      btnEmpezar.classList.remove('hide')
+    } else {
+      btnEmpezar.classList.add('hide')
+    }
+  }
+
   generarSecuencia() {
-    this.secuencia = new Array(10).fill(0).map(n => Math.floor(Math.random() * 4));
+    this.secuencia = new Array(ultimoNivel).fill(0).map(n => Math.floor(Math.random() * 4))
   }
   
   siguienteNivel() {
-    this.iluminarSecuencia();
-    this.agregarEventoClick();
+    this.subnivel = 0
+    this.iluminarSecuencia()
+    this.agregarEventoClick()
   }
   
   // Funcion que trasforma un numero a un color
@@ -46,10 +58,24 @@ class Juego {
     }
   }
 
+  // Funcion que transforma un color a un numero
+  trasformarColorANumero(color) {
+    switch(color){
+      case 'celeste':
+        return 0
+      case 'violeta':
+        return 1
+      case 'naranja':
+        return 2
+      case 'verde':
+        return 3
+    }
+  }
+
   // Funcion que recorre el numero de la secuencia y le aplica la funcion de transformarNumeroAColor
   iluminarSecuencia() {
     for(let i = 0; i < this.nivel; i++){
-      const color =this.trasformarNumeroAColor(this.secuencia[i])
+      let color = this.trasformarNumeroAColor(this.secuencia[i])
       setTimeout(() => this.iluminarColor(color), 1000 * i)
     }
   }
@@ -71,9 +97,44 @@ class Juego {
     this.colores.violeta.addEventListener('click', this.elegirColor)
     this.colores.naranja.addEventListener('click', this.elegirColor)
   }
+  
+  // Funcion que elimina los eventos click de los botones
+  eliminarEventoClick(){
+    this.colores.celeste.removeEventListener('click', this.elegirColor)
+    this.colores.verde.removeEventListener('click', this.elegirColor)
+    this.colores.violeta.removeEventListener('click', this.elegirColor)
+    this.colores.naranja.removeEventListener('click', this.elegirColor)
+  }
 
-  elegirColor(){
+  // Funcion para seleccionar el boton iluminado y seguir la secuencia
+  elegirColor(ev){
+    const nombreColor = ev.target.dataset.color
+    const numeroColor = this.trasformarColorANumero(nombreColor)
+    this.iluminarColor(nombreColor)
+    if(numeroColor === this.secuencia[this.subnivel]) {
+      this.subnivel++
+      if(this.subnivel === this.nivel) {
+        this.nivel++
+        this.eliminarEventoClick()
+        if (this.nivel === (ultimoNivel + 1)) {
+          this.ganoElJuego()
+        } else {
+          setTimeout(this.siguienteNivel, 1000)
+        }
+      }
+    } else {
+      this.perdioElJuego()
+    }
+  }
 
+  ganoElJuego(){
+    swal ( "Oops" ,  "Something went wrong!" ,  "success" )
+    .then(this.inicializar())
+  }
+
+  perdioElJuego(){
+    swal ( "Oops" ,  "Something went wrong!" ,  "error" )
+    .then(this.eliminarEventoClick())
   }
 }
 function empezarJuego(){
